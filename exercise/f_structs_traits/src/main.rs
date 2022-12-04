6d44e6bdc9cb4c57574b1d822103c5503c1ceb0e
Nicholas Stevenson <nstevenson@pubg.com>
2022-12-04 09:08:09 -0600

    Completed exercises E, F, G, and H

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -6,7 +6,6 @@
 //
 //  trait Bite...
 
-
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
@@ -30,9 +29,9 @@ fn main() {
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
     //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +40,30 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
+
+
+trait Bite {
+    fn bite(self: &mut Self) {}
+}
+
+fn bunny_nibbles<T: Bite>(item: &mut T) {
+    for _ in 1..10 {
+        item.bite();
+    }
+}
+
+#[derive(Debug)]
+struct Grapes {
+    amount_left: u32,
+}
+
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.amount_left -= 1;
+    }
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
@@ -56,3 +77,4 @@ impl Bite for Carrot {
         self.percent_left *= 0.8;
     }
 }
+
--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -1,5 +1,6 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_imports, unused_variables)]
+
 use crossbeam::channel;
 use std::thread;
 use std::time::Duration;
@@ -16,8 +17,8 @@ fn expensive_sum(v: Vec<i32>) -> i32 {
     // either need to dereference the argument once in the parameter list like this: `|&x|` or you
     // will need to dereference it each time you use it in the expression like this: `*x`
     v.iter()
-        // .filter() goes here
-        // .map() goes here
+        .filter(|&x| (x % 2) != 0)
+        .map(|&x| x * x)
         .sum()
 }
 
@@ -32,7 +33,7 @@ fn main() {
     // join handle in a variable called `handle`. Once you've done this you should be able to run
     // the code and see the Child thread output in the middle of the main thread's letters
     //
-    //let handle = ...
+    let handle = thread::spawn(|| { expensive_sum(my_vector) });
 
     // While the child thread is running, the main thread will also do some work
     for letter in vec!["a", "b", "c", "d", "e", "f"] {
@@ -40,13 +41,14 @@ fn main() {
         pause_ms(200);
     }
 
+
     // 3. Let's retrieve the value returned by the child thread once it has exited.  Using the
     // `handle` variable you stored the join handle in earlier, call .join() to wait for the thread
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
     //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+    let sum = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
@@ -56,7 +58,7 @@ fn main() {
     // flow of execution works.  Once you understand it, alter the values passed to the `pause_ms()`
     // calls so that both the "Thread B" outputs occur before the "Thread A" outputs.
 
-    /*
+
     let (tx, rx) = channel::unbounded();
     // Cloning a channel makes another variable connected to that end of the channel so that you can
     // send it to another thread.
@@ -72,7 +74,7 @@ fn main() {
     pause_ms(100); // Make sure Thread A has time to get going before we spawn Thread B
 
     let handle_b = thread::spawn(move || {
-        pause_ms(0);
+        pause_ms(100);
         tx.send("Thread B: 1").unwrap();
         pause_ms(200);
         tx.send("Thread B: 2").unwrap();
@@ -89,12 +91,30 @@ fn main() {
     // Join the child threads for good hygiene.
     handle_a.join().unwrap();
     handle_b.join().unwrap();
-    */
+
 
     // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
     // main thread loop through several values and print each out and then send it to the channel.
     // On the child threads print out the values you receive. Close the sending side in the main
     // thread by calling `drop(tx)` (assuming you named your sender channel variable `tx`).  Join
     // the child threads.
+
+    for letter in vec!["a", "b", "c", "d", "e", "f"] {
+        println!("Main thread: Letter {}", letter);
+        pause_ms(400);
+    }
+
+    let (t1, r1): (channel::Sender<&str>, channel::Receiver<&str>) = channel::unbounded();
+
+    let handle_1 = thread::spawn(move || {
+        t1.send("Thread 1: One").unwrap();
+    });
+
+    for msg in r1 {
+        println!("Main thread: Received {}", msg);
+    }
+    handle_1.join().unwrap();
+
+
     println!("Main thread: Exiting.")
 }
