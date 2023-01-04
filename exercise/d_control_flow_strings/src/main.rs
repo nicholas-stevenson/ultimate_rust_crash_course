222131e49770cf9b0a52032ecbad11d32162efaa
Nicholas Stevenson <nstevenson@bluehole.net>
2023-01-04 08:47:13 -0600

    Updating exercise D with a string literal match style instead of an if ladder

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -19,13 +19,11 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
-        if arg == "sum" {
-            sum();
-        } else if arg == "double" {
-            double();
-        } else {
-            count(arg);
-        };
+        match arg.as_str() {
+            "sum" => sum(),
+            "double" => double(),
+            _ => count(arg)
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
